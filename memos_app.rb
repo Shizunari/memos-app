f34df697b6ce036630a527a705af30799849f853
shizunari <mare.fe.2006+biz@gmail.com>
2026-08-30 00:35:54 +0900

    [Change]DBへの問合せをロードのメソッドを通るように変更

--- memos_app.rb
+++ memos_app.rb
@@ -7,14 +7,15 @@ require 'pg'
 
 DATABASE_NAME = 'memos_application_db'
 TABLE_NAME = 'memo_details'
+UUID_FORMAT = /\A\h{8}-\h{4}-\h{4}-\h{4}-\h{12}\z/
 
 configure do
   use Rack::Protection
   set :erb, escape_html: true
 end
 
-conn = PG.connect(dbname: DATABASE_NAME)
-conn.exec(<<~SQL)
+CONN = PG.connect(dbname: DATABASE_NAME)
+CONN.exec(<<~SQL)
   CREATE TABLE IF NOT EXISTS "#{TABLE_NAME}" (
     "memo_id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     "title" VARCHAR(255),
@@ -22,9 +23,17 @@ conn.exec(<<~SQL)
   )
 SQL
 
+def load_articles(file_id = nil)
+  if !file_id
+    CONN.exec("SELECT memo_id, title, content FROM \"#{TABLE_NAME}\" ORDER BY title DESC")
+  else
+    CONN.exec_params("SELECT memo_id, title, content FROM \"#{TABLE_NAME}\" WHERE memo_id = $1", [file_id])
+  end
+end
+
 get '/memos' do
   @page_title = '一覧表示'
-  @articles = conn.exec("SELECT memo_id, title, content FROM \"#{TABLE_NAME}\" ORDER BY title DESC")
+  @articles = load_articles
 
   erb :index
 end
@@ -36,7 +45,7 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  conn.exec_params(
+  CONN.exec_params(
     "INSERT INTO \"#{TABLE_NAME}\" (title, content) VALUES ($1, $2)",
     [params[:new_title], params[:new_content]]
   )
@@ -46,24 +55,30 @@ end
 
 get '/memos/:id' do
   @page_title = '個別表示'
-  result = conn.exec_params("SELECT memo_id, title, content FROM \"#{TABLE_NAME}\" WHERE memo_id = $1", [params[:id]])
-  halt 404 if result.ntuples.zero?
-  @article = result.first
+  halt 404 unless params[:id].match?(UUID_FORMAT)
+  article = load_articles(params[:id].to_s)
+  halt 404 if article.ntuples.zero?
+  @article = article.first
 
   erb :show
 end
 
 get '/memos/:id/edit' do
   @page_title = '編集画面'
-  result = conn.exec_params("SELECT memo_id, title, content FROM \"#{TABLE_NAME}\" WHERE memo_id = $1", [params[:id]])
-  halt 404 if result.ntuples.zero?
-  @article = result.first
+  halt 404 unless params[:id].match?(UUID_FORMAT)
+  article = load_articles(params[:id].to_s)
+  halt 404 if article.ntuples.zero?
+  @article = article.first
 
   erb :edit
 end
 
 patch '/memos/:id' do
-  conn.exec_params(
+  halt 404 unless params[:id].match?(UUID_FORMAT)
+  article = load_articles(params[:id].to_s)
+  halt 404 if article.ntuples.zero?
+
+  CONN.exec_params(
     "UPDATE \"#{TABLE_NAME}\" SET title = $1, content = $2 WHERE memo_id = $3",
     [params[:edit_title], params[:edit_content], params[:id]]
   )
@@ -72,7 +87,11 @@ patch '/memos/:id' do
 end
 
 delete '/memos/:id' do
-  conn.exec_params("DELETE FROM \"#{TABLE_NAME}\" WHERE memo_id = $1", [params[:id]])
+  halt 404 unless params[:id].match?(UUID_FORMAT)
+  article = load_articles(params[:id].to_s)
+  halt 404 if article.ntuples.zero?
+
+  CONN.exec_params("DELETE FROM \"#{TABLE_NAME}\" WHERE memo_id = $1", [params[:id]])
 
   redirect '/memos'
 end
